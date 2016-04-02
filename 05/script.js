c499c855600749fd0fa991f828487bfb72b2323e
omj1 <omj@o2.pl>
2016-04-02 17:51:43 +0200

    addded second exercise of 5. chapter

--- 05/script.js
+++ 05/script.js
@@ -14,19 +14,46 @@ tab3.forEach(function(number) {
 });
 console.log(sum3);
 
-// reduce method
+// reduce method not working well
 var sum4 = [];
 var x;
 function sum(numbers, pos) {
-  console.log(numbers);
-  sum4.unshift(pos);
+  // console.log(numbers);
+  sum4.push(pos);
   return sum4;
 }
 console.log(sum3.reduce(sum));
 
+//reduce metheod
+var sum5 = [];
+sum3.reduce(function(numbers, pos) { sum5.push(pos); }, 0);
+console.log(sum5);
 
-var numbersi = [65, 44, 12, 4];
-function getSum(total, num) {
-    return total + num;
+
+//diffrent in ages between mother and kid
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+var mother,
+    bdaykid,
+    bdaymother;
+
+function findsamename(name) {
+  var name2 = name;
+  for (var i = 0; i < ancestry.length; i++ ) {
+      if (ancestry[i].name == name2) {
+      mother = ancestry[i].mother;
+      bdaykid = ancestry[i].born;
+      console.log(bdaykid);
+    }
+  }
+
+  for (var j = 0; j < ancestry.length; j++ ) {
+      if (ancestry[j].name == mother) {
+      bdaymother = ancestry[j].born;
+      console.log(ancestry[j].born);
+    }
+  }
+  console.log(bdaykid - bdaymother);
 }
-console.log(numbersi.reduce(getSum));
+
+findsamename("Daniel Haverbeke");
